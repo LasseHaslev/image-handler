b239d17ca77eae586d196b4a33454212768e06da
Lasse S. Haslev <lasse@haslev.no>
2016-06-25 00:42:54 +0200

    Made test aware of test image

--- tests/ImageHandlerTest.php
+++ tests/ImageHandlerTest.php
@@ -10,16 +10,18 @@ class ImageHandlerTest extends PHPUnit_Framework_TestCase
 {
 
     protected $handler;
+    protected $imagePath;
 
     public function setup()
     {
+        $this->imagePath = __DIR__ . '/../images/test-image.jpg';
         $this->handler = new ImageCropHandler();
     }
 
 
     public function test_image_resize()
     {
-        return $this->assertTrue( true );
+        return $this->assertTrue( file_exists( $this->imagePath ) );
     }
 
 }
