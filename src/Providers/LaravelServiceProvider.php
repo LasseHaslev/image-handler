4a512c750118ffc2bc8f6bbdfb1327cf078397bf
Lasse S. Haslev <lasse@haslev.no>
2016-06-26 18:54:40 +0200

    Added laravel service provided. Not tested

---
+++ src/Providers/LaravelServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace LasseHaslev\Image\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        $router = $this->app[ 'router' ];
+
+        $router->get( '{path}', $this->handleUrl )
+            ->where( 'path', '(.+)-([0-9_]+)x([0-9_]+)(-[0-9a-zA-Z(),\-._]+)*\.(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$' );
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Handle the image url
+     *
+     * @return void
+     */
+    public function handleUrl( $url )
+    {
+        $cropHandler = new ImageCropHandler;
+        return $cropHandler->handleUrl( $url )
+            ->getResponse();
+    }
+
+    /**
+     * Download the original image
+     *
+     * @return Response
+     */
+    public function download( $image )
+    {
+        return response()->download( $image->fullPath, $image->name );
+    }
+}
