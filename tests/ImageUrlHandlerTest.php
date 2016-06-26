fef2d597a3cdfdd7e7e27962cdf810d5fe8a4d4b
Lasse S. Haslev <lasse@haslev.no>
2016-06-26 15:54:26 +0200

    Added relative path

---
+++ tests/ImageUrlHandlerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use LasseHaslev\Image\Handlers\ImageUrlHandler;
+
+/**
+ * Class ImageUrlHandlerTest
+ * @author Lasse S. Haslev
+ */
+class ImageUrlHandlerTest extends PHPUnit_Framework_TestCase
+{
+
+    protected $fileHandler;
+    protected $imageFolder;
+
+    public function setup()
+    {
+        $this->imageFolder = __DIR__ . '/..';
+        $this->fileHandler = new ImageUrlHandler( $this->imageFolder );
+    }
+
+    function test_get_relative_url_to_image_path()
+    {
+
+        $url = '/images/test-image-70x70.jpg';
+        $filename = $this->imageFolder . '/images/test-image-70x70.jpg';
+        $this->fileHandler->handle( $url );
+        $this->assertFileExists( $filename );
+    }
+
+}
