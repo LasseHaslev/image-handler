13bd4273cb9e0d82430704763f44c908985fc840
Lasse S. Haslev <lasse@haslev.no>
2017-02-21 11:56:53 +0100

    Got new image size for small image

--- prepare.sh
+++ prepare.sh
@@ -4,4 +4,4 @@ mkdir images/tests
 
 wget http://placehold.it/350x350/ffffff/000000 -O images/placehold.jpg
 wget http://placekitten.com/g/1280/800 -O images/kitten1.jpg
-wget http://placekitten.com/g/200 -O images/kitten-small.jpg
+wget http://placekitten.com/g/200/150 -O images/kitten-small.jpg
