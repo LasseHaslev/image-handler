ee910ef278a868b81309538d0bfada1e87e701ca
Lasse S. Haslev <lasse@haslev.no>
2017-04-28 11:16:21 +0200

    Added test to check if filename adaptor could handle resize flag

--- tests/FilenameAdaptorTest.php
+++ tests/FilenameAdaptorTest.php
@@ -3,6 +3,7 @@
 use LasseHaslev\Image\Handlers\ImageHandler;
 use LasseHaslev\Image\Adaptors\FilenameAdaptor;
 use LasseHaslev\Image\Handlers\CropHandler;
+use LasseHaslev\Image\Helpers\ImageFilename;
 
 /**
  * Class DontKnowTest
@@ -25,7 +26,8 @@ class FilenameAdaptorTest extends PHPUnit_Framework_TestCase
         $this->handler = CropHandler::create( __DIR__ . '/../images' );
         $this->handler->setAdaptor( new FilenameAdaptor );
         $this->handler->setCropsFolder(__DIR__ . '/../images/tests/FilenameAdaptor' );
-        $this->imagePath = __DIR__ . '/../images/kitten1.jpg';
+        $this->imageName = 'kitten1.jpg';
+        $this->imagePath = __DIR__ . '/../images/' + $this->imageName;
         // $this->modifier = ImageHandler::create( $this->imagePath );
     }
 
@@ -53,20 +55,76 @@ class FilenameAdaptorTest extends PHPUnit_Framework_TestCase
 
         $width = 200;
         $height = 200;
-        $filename = $this->getImageName( $width, $height );
+        $filename = ImageFilename::filename( $this->imageName, [
+            'width'=>$width,
+            'height'=>$height,
+        ] );
 
         $this->handler
             ->handle( $filename )
             ->save( $filename );
 
         $size = getimagesize( $this->handler->getCropsFolder( $filename ) );
-
-        /**
-         * also count it yourself, as I can't reproduce your code
-         */
         $this->assertEquals($width, $size[0]);
         $this->assertEquals($height, $size[1]);
 
     }
+
+    // resize by width
+    /** @test */
+    public function can_resize_image_by_width() {
+        $width = 200;
+        $filename = ImageFilename::filename( $this->imageName, [
+            'width'=>$width,
+        ] );
+
+        $this->handler
+            ->handle( $filename )
+            ->save( $filename );
+
+        $size = getimagesize( $this->handler->getCropsFolder( $filename ) );
+        $this->assertEquals($width, $size[0]);
+        $this->assertNotEquals(800, $size[1]);
+    }
+
+    /** @test */
+    public function can_resize_image_by_height() {
+        $height = 200;
+        $filename = ImageFilename::filename( $this->imageName, [
+            'height'=>$height,
+        ] );
+
+        $this->handler
+            ->handle( $filename )
+            ->save( $filename );
+
+        $size = getimagesize( $this->handler->getCropsFolder( $filename ) );
+        $this->assertNotEquals(1280, $size[0]);
+        $this->assertEquals($height, $size[1]);
+    }
+    // resize with flag
+
+    /** @test */
+    public function can_resize_image_by_resize_flag_when_width_and_height_is_set() {
+        $width = 200;
+        $height = 200;
+        $filename = ImageFilename::filename( $this->imageName, [
+            'width'=>$width,
+            'height'=>$height,
+            'resize'=>true,
+        ] );
+
+        $this->handler
+            ->handle( $filename )
+            ->save( $filename );
+
+        $size = getimagesize( $this->handler->getCropsFolder( $filename ) );
+        $this->assertEquals($width, $size[0]);
+        $this->assertEquals(125, $size[1]);
+    }
+
+    // Set focus point X
+    // Set focus point Y
+    // Set focus point X and Y
 
 }
